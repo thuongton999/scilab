6fa3144223a6130f1cb731adc18b654bb16aaa1f
thuongton999 <thuongton0809999@gmail.com>
2023-02-13 02:04:57 +0700

    init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image(), robotsTxt()]
+});

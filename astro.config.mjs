d070a0ae3e1222055b3aa9eda936865449040a34
thuongton999 <thuongton0809999@gmail.com>
2023-02-13 06:42:53 +0700

    add hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
 
+import { astroImageTools } from "astro-imagetools";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), robotsTxt()]
+  integrations: [image(), robotsTxt(), astroImageTools]
 });

80b9cd01b4816bce6f35067e2f976904b297ad76
thuongton999 <thuongton0809999@gmail.com>
2023-02-26 14:38:01 +0700

    use next gen image format

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro-imagetools/config";
 
 export default defineConfig({
     loading: "lazy",
+    format: ["avif", "webp"],
+    fallbackFormat: "png",
     objectFit: "cover",
     objectPosition: "50% 50%",
     layout: "fill"

b0e1911f4a5e3a84a5831a5a328049df0ae18a00
thuongton999 <thuongton0809999@gmail.com>
2023-02-26 21:24:34 +0700

    update prefer image type

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro-imagetools/config";
 
 export default defineConfig({
     loading: "lazy",
-    format: ["avif", "webp"],
+    format: ["webp", "jpeg", "jpg", "gif"],
     fallbackFormat: "png",
     objectFit: "cover",
     objectPosition: "50% 50%",

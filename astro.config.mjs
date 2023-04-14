d21003afb35e9f110da4710687bc0af72a1fd5e0
thuongton999 <thuongton0809999@gmail.com>
2023-04-15 01:18:15 +0700

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import robotsTxt from "astro-robots-txt";
 import { astroImageTools } from "astro-imagetools";
 
-// https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
+// import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 import react from "@astrojs/react";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: "https://scilab.netlify.app",
   integrations: [robotsTxt(), astroImageTools, react()],
-  adapter: netlify({
-    builders: true
-  })
+  adapter: vercel()
 });
